43797c786504d34bd2477ab009e40a97eb7f9ab2
Efrata Yiehalem <kukuyiehalem@gmail.com>
2025-09-14 13:55:38 +0300

    Fix 8-square.js to not print anything for negative size

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (args[0] === undefined) {
+    console.log("Missing size");
+} else if (size > 0) {
+    let i = 0;
+    while (i < size) {
+        console.log("X".repeat(size));
+        i++;
+    }
+}
+// If size <= 0 or NaN, print nothing
